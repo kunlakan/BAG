d73d3cb31d17e78d644956ce3860b6ecb0bbed39
Kunlakan (Jeen) Cherdchusilp <kunlakan@uw.edu>
2018-03-03 09:04:57 -0800

    Initial worker.py

---
+++ worker.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content == "Hello":
+        await
+        client.send_message(message.channel, "World")
+
+
+client.run(NDE5NTMxOTMxNTk2NzUwODU4.DXxkQA.y_ybewiQYtRpo_TPTSn5Qo5Txwk)
